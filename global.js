682e8c89bcbe831d662b6254f23d6e43f30721fb
Zoey He <jih084@ucsd.edu>
2025-01-24 20:07:17 -0800

    I forgot what I did but save

--- global.js
+++ global.js
@@ -4,13 +4,41 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-// 2.1
-let navLinks = $$("nav a");
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'https://github.com/jih084', title: 'GitHub' },
+  ];
+  
+//step 3.1 Adding the navigation menu
 
-//2.2
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname
-  );
+let nav = document.createElement('nav');
+document.body.prepend(nav);
 
-//2.3
-currentLink?.classList.add('current');
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+
+    const ARE_WE_HOME = document.documentElement.classList.contains('home');
+    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+
+//step 3.2  Highlighting the current page and opening external links in a new tab
+
+    a.classList.toggle(
+    'current',
+    a.host === location.host && a.pathname === location.pathname
+    );
+    
+    a.toggleAttribute('target', a.host !== location.host);
+    a.target = '_blank';
+
+
+    nav.append(a);
+}
+
